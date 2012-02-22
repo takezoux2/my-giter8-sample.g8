14950411ca7a5c506ac283db2d295c1a05b25191
takeshita <takeshita@geishatokyo.com>
2012-02-22 20:21:22 +0900

    format samples

--- src/main/g8/src/main/scala/Stub.scala
+++ src/main/g8/src/main/scala/Stub.scala
@@ -2,4 +2,10 @@ class Stub {
   // got any helpful boilerplate for your users?
   
   def aa() = "aa"
+  
+  $symbol; format="capitalize"$
+  $symbol; format="cap"$
+  $symbol; format="upper"$
+  $symbol; format="capitalize"$
+  $symbol; format="packaged"$
 }
